be84de008c46dae323c3ba14ced86e44ac960b6a
Marcos Carvalho <marc.carvalho98@gmail.com>
2023-02-02 16:24:43 -0300

    OPERAÇÕES DATA E HORA + DATE/CALENDAR JAVA7

--- CalculosDataHora/src/application/Program.java
+++ CalculosDataHora/src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Program {
 
@@ -14,8 +17,8 @@ public class Program {
 		LocalDateTime d05 = LocalDateTime.parse("2023-02-01T14:25:30");
 		Instant d06 = Instant.parse("2023-02-01T00:10:30Z");
 		
-		LocalDate pastWeekLocalDate = d04.minusDays(7);
-		LocalDate nextWeekLocalDate = d04.plusDays(7);
+		LocalDate pastWeekLocalDate = d04.minusDays(7); //ADICIONANDO
+		LocalDate nextWeekLocalDate = d04.plusDays(7); // SUBTRAINDO
 		
 		
 		System.out.println("pastWeekLocalDate : " + pastWeekLocalDate);
@@ -36,6 +39,7 @@ public class Program {
 		System.out.println("pastWeekInstant: " + pastWeekInstant);
 		System.out.println("nexttWeekInstant: " + nextWeekInstant);
 		
+		//TEMPO ENTRE DUAS DATAS
 		Duration t1 = Duration.between(pastWeekLocalDateTime, d05);
 		Duration t2 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
 		Duration t3 = Duration.between(pastWeekInstant, d06);
@@ -45,7 +49,28 @@ public class Program {
 		System.out.println("t2 dias: " + t2.toDays());
 		System.out.println("t3 dias: " + t3.toDays());
 		System.out.println("t4 dias: " + t4.toDays());
+		System.out.println();
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("DATA/CALENDAR JAVA7");
+		System.out.println();
+		//DATA E HORA JAVA7
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		//CALCULOS
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		int minutes = cal.get(Calendar.MINUTE); //OBTENDO INFORMAÇÕES DA DATA
+		int month = 1 + cal.get(Calendar.MONTH); //JANEIRO É MÊS 0 POR ISSO ADICIONA 1
+		
+		System.out.println("Calendar 1: " + sdf.format(d));
+		System.out.println("Minutes: " + minutes);
+		System.out.println("MONTH: " + month);
 		
 
 	}
